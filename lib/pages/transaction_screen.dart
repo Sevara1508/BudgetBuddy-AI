f56ae35127c9e6079a73eca7ead92a1f2c9bdbdb
Tisha Patel <tishapatel188@gmail.com>
2025-12-02 00:37:52 -0500

    deleting transactions

--- lib/pages/transaction_screen.dart
+++ lib/pages/transaction_screen.dart
@@ -70,17 +70,33 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
         const SnackBar(
           content: Text("Transaction added!"),
           backgroundColor: Colors.green,
-          ),
+        ),
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text("Failed to add transaction!"),
           backgroundColor: Colors.red,
-          ),
+        ),
       );
     }
   }
+
+  // create function to deletes a transaction
+  Future<void> _deleteTransaction(int? id) async {
+    if (id == null) return;
+
+    await _dbHelper.deleteTransaction(id);
+    await _loadTransact();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Transaction deleted"), // notif
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,13 +125,11 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                     value: _selectCategory,
                     hint: const Text('Select'),
                     items: _categories
-                    // to create dropdown items from the categories list
                         .map((category) => DropdownMenuItem(
                       value: category,
                       child: Text(category),
                     ))
                         .toList(),
-                    // when user selects a category from dropdown
                     onChanged: (value) {
                       setState(() {
                         _selectCategory = value;
@@ -133,21 +147,18 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                   const Text('Date: '),
                   TextButton(
                     onPressed: () async {
-                      // open date picker from flutter library
                       final chosenDate = await showDatePicker(
                         context: context,
                         initialDate: DateTime.now(),
                         firstDate: DateTime(2004),
                         lastDate: DateTime(2100),
                       );
-                      // if user selected a date, save it
                       if (chosenDate != null) {
                         setState(() {
                           _selectDate = chosenDate;
                         });
                       }
                     },
-                    // display the selected date or prompt to choose one
                     child: Text(
                       _selectDate == null
                           ? 'Select Date'
@@ -156,7 +167,6 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                   ),
                 ],
               ),
-
 
               const SizedBox(height: 10),
 
@@ -170,7 +180,6 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
 
               const SizedBox(height: 10),
 
-              // create amount input dialog
               Row(
                 children: [
                   const Text('Amount: '),
@@ -178,7 +187,6 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                     onPressed: () async {
                       final controller = TextEditingController();
 
-                      // show dialog to enter an amount
                       final result = await showDialog<double>(
                         context: context,
                         builder: (context) {
@@ -190,7 +198,6 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                               decoration:
                               const InputDecoration(labelText: 'Amount'),
                             ),
-                            // save button to return the entered amount
                             actions: [
                               TextButton(
                                 onPressed: () {
@@ -199,20 +206,19 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                                     double.tryParse(controller.text) ?? 0.0,
                                   );
                                 },
-                                child: const Text('Save'), // display "Save" button
+                                child: const Text('Save'),
                               ),
                             ],
                           );
                         },
                       );
-                      // if user entered a valid amount then update it
+
                       if (result != null) {
                         setState(() {
                           _enterAmount = result;
                         });
                       }
                     },
-                    // display the entered amount or prompt user to enter one
                     child: Text(
                       _enterAmount == null
                           ? 'Enter Amount'
@@ -222,9 +228,8 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                 ],
               ),
 
-
               const SizedBox(height: 20),
-              //button to save transaction
+
               ElevatedButton(
                 onPressed: _addTransaction,
                 child: const Text('Save Transaction'),
@@ -238,7 +243,6 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
               ),
               const SizedBox(height: 20),
 
-              //code for the saved transactions
               ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
@@ -247,15 +251,29 @@ class _Transaction_ScreenState extends State<Transaction_Screen> {
                   var inp = _transactions[index];
                   return Card(
                     color: Theme.of(context).brightness == Brightness.dark
-                        ? const Color(0xFF3B2F4A)    // dark purple-grey
-                        : const Color(0xFFEDE7F6),   // light lavender
+                        ? const Color(0xFF3B2F4A)
+                        : const Color(0xFFEDE7F6),
                     child: ListTile(
                       title: Text(
                         inp.category,
                         style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                       subtitle: Text(inp.note ?? ''),
-                      trailing: Text('\$${inp.amount.toStringAsFixed(2)}'), //displays amount in decimal
+
+                      // updated to also delete transaction
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('\$${inp.amount.toStringAsFixed(2)}'), //displays amount in decimal
+                          const SizedBox(width: 12),
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              _deleteTransaction(inp.id);
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
